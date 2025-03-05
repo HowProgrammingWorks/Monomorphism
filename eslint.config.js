ca4b4c9cb7612381953417d1fa25aa3cd96edb44
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2025-03-05 17:47:58 +0200

    Add TypeScript

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,6 @@ const init = require('eslint-config-metarhia');
 
 init[0].rules['no-sparse-arrays'] = 'off';
 init[0].rules['no-undef'] = 'off';
+init[0].rules['consistent-return'] = 'off';
 
 module.exports = init;

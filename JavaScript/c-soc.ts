944262d1e91a47643e39e29409d2747b743daca0
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2025-03-07 15:38:08 +0200

    Add fixed ts example

---
+++ JavaScript/c-soc.ts
@@ -0,0 +1,25 @@
+type AdderMapFunction = (value: number) => number;
+
+class Adder {
+  #value: number;
+
+  constructor(initial: number = 0) {
+    this.#value = initial;
+  }
+
+  add(delta: number): void {
+    this.#value += delta;
+  }
+
+  get(): number {
+    return this.#value;
+  }
+
+  map(fn: AdderMapFunction): number {
+    return fn(this.#value);
+  }
+
+  set(value: number): void {
+    this.#value = value;
+  }
+}
